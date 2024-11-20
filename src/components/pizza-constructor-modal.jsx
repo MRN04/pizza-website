1fed3ae52235f231e60178e78ff0cf2e475e5baf
Maksym Peretiatko <maksym.peretiatko@lnu.edu.ua>
2024-11-20 16:17:13 +0200

    feat: searchym paramsy

--- src/components/pizza-constructor-modal.jsx
+++ src/components/pizza-constructor-modal.jsx
@@ -1,95 +1,9 @@
 import { useAtom } from "jotai"
 import "../css/pizza-constructor.css"
 import { isOpenModal, userPizzaInitial, userPizzasInitial, isPizzaToChange, isPizzaToAdd, pizzaToChangeInitial } from "../store/atoms"
-import { useRef, useState } from "react"
+import { useState } from "react"
 
-const ingredients = [
-    {
-        name: "Курка",
-        price: 10,
-        index: "1",
-    },
-    {
-        name: "Моцарела",
-        price: 10,
-        index: "2",
-    },
-    {
-        name: "Груша",
-        price: 10,
-        index: "3",
-    },
-    {
-        name: "Індичка",
-        price: 10,
-        index: "4",
-    },
-    {
-        name: "Крем-сир Філадельфія",
-        price: 10,
-        index: "5",
-    },
-    {
-        name: "Ковбаски баварські",
-        price: 10,
-        index: "6",
-    },
-    {
-        name: "Гриби",
-        price: 10,
-        index: "7",
-    },
-    {
-        name: "Цибуля",
-        price: 10,
-        index: "8",
-    },
-    {
-        name: "Кукурудза",
-        price: 10,
-        index: "9",
-    },
-    {
-        name: "Пепероні",
-        price: 10,
-        index: "10",
-    },
-    {
-        name: "Помідори",
-        price: 10,
-        index: "11",
-    },
-    {
-        name: "Шинка",
-        price: 10,
-        index: "12",
-    },
-    {
-        name: "Бекон",
-        price: 10,
-        index: "13",
-    },
-    {
-        name: "Огірки мариновані",
-        price: 10,
-        index: "14",
-    },
-    {
-        name: "Бергадер Блю",
-        price: 10,
-        index: "15",
-    },
-    {
-        name: "Фета",
-        price: 10,
-        index: "16",
-    },
-    {
-        name: "Шпинат",
-        price: 10,
-        index: "17",
-    },
-]
+
 
 
 
